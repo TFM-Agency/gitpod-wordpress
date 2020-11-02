2329ab3064297ba13f719ab1ad40bae4b35256eb
Tobias Feistmantl <tobias@feistmantl.io>
2020-11-02 11:08:18 +0100

    Implement plugin install function for bash and install wp in project dir alongside project files

--- conf/.bashrc.sh
+++ conf/.bashrc.sh
@@ -1,6 +1,8 @@
 
 # WordPress Setup Script
 export REPO_NAME=$(basename $GITPOD_REPO_ROOT)
+export WP_INSTALL_PATH=${GITPOD_REPO_ROOT}/wp-install
+export PROJECT_PATH=${WP_INSTALL_PATH}/wp-content/plugins/${REPO_NAME}
 
 function wp-init-database () {
   # user     = wordpress
@@ -27,8 +29,8 @@ function wp-setup () {
   shopt -s dotglob
   
   # move the workspace temporarily
-  mkdir $HOME/workspace
-  mv ${GITPOD_REPO_ROOT}/* $HOME/workspace/
+  # mkdir $HOME/workspace
+  # mv ${GITPOD_REPO_ROOT}/* $HOME/workspace/
   
   # create a debugger launch.json
   mkdir -p ${GITPOD_REPO_ROOT}/.theia
@@ -38,34 +40,36 @@ function wp-setup () {
   echo 'Creating MySQL user and database ...'
   wp-init-database 1> /dev/null
   
+  
+
   # install WordPress
   echo 'Installing WordPress ...'
-  rm -rf ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
-  mkdir -p ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
-  cd ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
+  rm -rf $WP_INSTALL_PATH
+  mkdir -p $WP_INSTALL_PATH
+  cd $WP_INSTALL_PATH
   wp core download
-  cp $HOME/gitpod-wordpress/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+  cp $HOME/gitpod-wordpress/conf/wp-config.php ${WP_INSTALL_PATH}/wp-config.php
   wp core install \
     --url="$(gp url 8080 | sed -e s/https:\\/\\/// | sed -e s/\\///)" \
     --title="WordPress" \
     --admin_user="admin" \
-    --admin_password="password" \
+    --admin_password="123" \
     --admin_email="admin@gitpod.test"
     
   echo 'Downloading Adminer ...'
-  mkdir ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/database/
-  wget -q https://www.adminer.org/latest.php -O ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/database/index.php
+  mkdir ${WP_INSTALL_PATH}/database/
+  wget -q https://www.adminer.org/latest.php -O ${WP_INSTALL_PATH}/database/index.php
   
   echo 'Creating phpinfo() page ...'
-  mkdir ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/phpinfo/
-  echo "<?php phpinfo(); ?>" > ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/phpinfo/index.php
+  mkdir ${WP_INSTALL_PATH}/phpinfo/
+  echo "<?php phpinfo(); ?>" > ${WP_INSTALL_PATH}/phpinfo/index.php
 
   # put the project files in the correct place
-  echo 'Creating project files ...'
-  PROJECT_PATH=${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-content/$1/${REPO_NAME}
-  mkdir -p $PROJECT_PATH
-  mv $HOME/workspace/* ${PROJECT_PATH}
-  cd $DESTINATION
+  # echo 'Creating project files ...'
+  # PROJECT_PATH=${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-content/$1/${REPO_NAME}
+  # mkdir -p $PROJECT_PATH
+  # mv $HOME/workspace/* ${PROJECT_PATH}
+  # cd $DESTINATION
 
   # install project dependencies
   if [ -f composer.json ]; then
@@ -80,11 +84,11 @@ function wp-setup () {
 
   if [ -f ${PROJECT_PATH}/.init.sh ]; then
     echo '.init.sh detected ...'
-    cp ${PROJECT_PATH}/.init.sh ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/.init.sh
+    cp ${PROJECT_PATH}/.init.sh ${WP_INSTALL_PATH}/.init.sh
     echo 'Running your .init.sh ...'
-    cd ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
-    /bin/bash ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/.init.sh
-    rm -rf ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/.init.sh
+    cd ${WP_INSTALL_PATH}/
+    /bin/bash ${WP_INSTALL_PATH}/.init.sh
+    rm -rf ${WP_INSTALL_PATH}/.init.sh
   fi
   
   # finish
@@ -100,6 +104,19 @@ function wp-setup-theme () {
 
 function wp-setup-plugin () {
   wp-setup "plugins"
+}
+
+function wp-install-plugin () {
+  echo 'Installing plugin in WordPress ...'
+  mkdir -p $PROJECT_PATH
+  cd ${GITPOD_REPO_ROOT}
+  cp -r !(wp-install) ${PROJECT_PATH}
+  cd $DESTINATION
+}
+
+function wp-update-gitignore () {
+  echo "wp-install/" >> $GITPOD_REPO_ROOT/.gitignore
+  echo "*.log" >> $GITPOD_REPO_ROOT/.gitignore
 }
 
 export -f wp-setup-theme
